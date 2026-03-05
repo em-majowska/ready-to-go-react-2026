87be45ac388dedc666f7fc9a2ddd73922e99effe
Ewa Majowska <e.majowska@etik.com>
2026-03-05 10:06:56 +0400

    Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import logo from "../assets/rocket.svg";
+
 const Header = () => {
   return (
     <header>
       <div className="container">
+        <img src={logo} alt="rocket logo" />
         <h1>Ready To Go</h1>
       </div>
     </header>
